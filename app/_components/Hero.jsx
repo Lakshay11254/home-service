909380704be6235712b1a541d95650b143fed1fc
Lakshay <lakshay.sbp@gmail.com>
2024-03-03 16:59:07 -0500

    Started Creating Hero Section

---
+++ app/_components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="flex items-center flex-col justify-center pt-14 pb-7">
+      <h2 className="font-bold text-[46px] text-center">
+        Find Home <span className="text-purple-800"> Services/Repair </span>{" "}
+        <br /> Near You
+      </h2>
+      <h2 className="text-xl text-gray-400">Explore Best Home Services & Repair Near You</h2>
+    </div>
+  );
+}
+
+export default Hero;
